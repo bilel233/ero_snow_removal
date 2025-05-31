f4371edb18eaba9898ff84bf87062cc4d8525dd0
bilel.majdoub <bilel.majdoub@epita.fr>
2025-05-31 12:24:12 +0200

    feat: creation du script setup_env.sh

---
+++ scripts/setup_env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+# on cree l'environnement venv
+python3 -m venv .venv
+source .venv/bin/activate
+
+# On fait la mise à jour de pip et  wheel
+python -m pip install --upgrade pip wheel
+
+# Installation des dépendances a partir du fichier requirements
+pip install -r ../requirements.txt
